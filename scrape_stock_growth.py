b1c840bc5e4d22066c3a88079a5a3825d5ed9013
CS70 Chimp <rchen@cs.hmc.edu>
2018-04-04 16:07:11 -0700

    stock growth

---
+++ scrape_stock_growth.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+from decimal import *
+
+def get_stock_page(symbol):
+  url = "https://finance.yahoo.com/quote/" + symbol \
+      + "/history?period1=1325404800&period2=1514793600&" \
+      + "interval=1mo&filter=history&frequency=1mo"
+  response = requests.get(url)
+
+  if response.status_code != 404:
+    soup = BeautifulSoup(response.text, "lxml")
+
+  return soup
+
+def find_stock_prices(soup):
+  stock_prices = {}
+  this_year = datetime.datetime.now().year
+  table = soup.find('tbody')
+  for row in table.find_all('tr'):
+    year = int(row.find_all('span')[0].text[-4:])
+    price = Decimal(row.find_all('span')[4].text)
+    if year not in stock_prices and year != this_year:
+      stock_prices[year] = price
+  return stock_prices
+
+def calculate_growth(stock_prices):
+  stock_growth = {}
+  for key in stock_prices:
+    if key - 1 in stock_prices:
+      stock_growth[key] = growth_calculate(
+        stock_prices[key], stock_prices[key-1])
+  # early = min(stock_growth.keys())
+  # del stock_growth[early]
+  return stock_growth
+
+def scrape_stock_growth(symbol):
+  soup = get_stock_page(symbol)
+  stock_prices = find_stock_prices(soup)
+  stock_growth = calculate_growth(stock_prices)
+  return stock_growth
+
+def growth_calculate(second, first):
+  return (second - first)/first
+
+def main():
+  print scrape_stock_growth("BABA")
+
+if __name__ == "__main__":
+    main()  # hike!
