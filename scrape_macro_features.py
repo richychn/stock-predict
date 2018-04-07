3c3e8abe17eea67afe7743f997ac56ac84609041
CS70 Chimp <rchen@cs.hmc.edu>
2018-04-06 17:31:05 -0700

    macro

--- scrape_macro_features.py
+++ scrape_macro_features.py
@@ -1,14 +1,27 @@
 import requests
+import string
+import json
+import numpy
+import decimal
 from bs4 import BeautifulSoup
 
 def get_observation_ur(industry, year):
   key = "89c89a90854ea7514f605a1af848a7f6"
-  series = ur_industries[industry]
+  series = ur_industries(industry)
   url = "https://api.stlouisfed.org/fred/series/observations?series_id=" \
       + series + "&api_key=" + key + "&file_type=json"
+  result = requests.get(url)
+  data = result.json()
+  ret = 0
+  count = 0
+  for ob in data['observations']:
+    if str(ob['date'][0:4]) == str(year):
+      ret += decimal.Decimal(ob['value'])
+      count += 1
+  return ret / count
 
-def ur_industries():
-  return {"manufacturing": "LNU04032232",
+def ur_industries(industry):
+  industries = {"manufacturing": "LNU04032232",
           "constructing": "LNU04032231",
           "finance": "LNU04032238",
           "business": "LNU04032239",
@@ -21,3 +34,10 @@ def ur_industries():
           "other": "LNU04032242",
           "transportation": "LNU04032236"
           }
+  return industries[industry]
+
+def main():
+  print(get_observation_ur("mining", 2009))
+
+if __name__ == "__main__":
+    main()  # hike!
