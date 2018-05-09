e934311f929fcfb1822517160c90f5ed6b1b9140
TonyJiang17 <tjiang@g.hmc.edu>
2018-05-09 16:23:17 -0700

    finished basics

--- app/controllers/predict_controller.rb
+++ app/controllers/predict_controller.rb
@@ -1,9 +1,19 @@
+require 'yaml'
 class PredictController < ApplicationController
   def new
   	#nothing to do 
   end
 
   def learn
-  	@input = 
+  	@input = params[:ticker]
+    result = %x[ipython app/assets/backend/alltogether.py #{@input}]
+    l = result.split(']')
+    l = l[l.length-2]
+    l = l.split('[')
+    @output = '[' + l[l.length-1] + ']'
+    @output = YAML.load(@output)
+    @model = @output[0]
+    @confidence = @output[1]
+    @result = @output[2]   
   end
 end
