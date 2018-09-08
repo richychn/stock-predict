e870faa8cb4e57bf4ca4118599b6ca8c9507114d
TonyJiang17 <tjiang@g.hmc.edu>
2018-09-07 17:11:06 -0700

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   resources :predictions, only: [:index, :show, :new, :create, :destroy]
   get 'error', to: 'predictions#error', as: :error_path
+  root 'predictions#index'
   
 end
